7e1999897295e4fd1b7b598f9dc2f7a0497f8781
anton <avainfoconseils@gmail.com>
2025-06-12 17:00:27 +0200

    IDK

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-const products = []
+const products = [
+	{
+		"name": "test",
+		"price": 2
+	},
+	{
+		"name": "test",
+		"price": 23
+	},
+	{
+		"name": "test",
+		"price": 234
+	},
+	{
+		"name": "test",
+		"price": 2345
+	},
+	{
+		"name": "test",
+		"price": 23456
+	},
+	{
+		"name": "test",
+		"price": 234567
+	}
+]
 
 function appendProduct() {
 	let name = document.getElementById('product-name').value
@@ -32,4 +57,41 @@ function appendProduct() {
 		}
 	}
 	console.log(products)
+}
+
+// document // Selectionner la page HTML
+// document.getElementById("") // Selectionner un élement par son ID
+// document.querySelector("button[type='submit']") // Selectionner un élement par son sélecteur
+// document.getElementById("").append() // Ajouter un enfant à un parent selectionner
+// document.getElementById("").addEventListener('click', (e) => {}) // Ajout d'un event
+
+function showProducts() {
+	// let container = document.getElementsByClassName('container')[0]
+	let container = document.querySelector('.container');
+
+	console.log(container)
+
+	// Créer un container pour contenir nos p
+	// pour que si on le supprime ca supprime tous nos p dedans
+
+	// Ajouter une classe "p-temp" pour les paragraphes temporaire et de
+	// tous les supprimer
+
+	// Verifier si les p existe, si c'est le cas ne pas rajouter
+
+	// Supprimer les p qui contiennent ": " + "€"
+
+	let paragraphElements = [...container.getElementsByTagName("p")]
+
+	paragraphElements.forEach((element) => {
+		container.removeChild(element)
+	})
+
+	for(const product of products) {
+		let node = document.createElement("p")
+		node.classList.add("p-temp")
+		node.innerText = product.name + ": " + product.price + "€"
+		container.appendChild(node)
+	}
+
 }
